db18a53ba7246059ee4317122d2ec3acfca78024
Tian <123768900+TianB33@users.noreply.github.com>
2024-04-04 19:04:27 -0400

    modify countprediction to predict only 1 gene at a time; add small value to beta/gamma to prevent nan

--- model.py
+++ model.py
@@ -23,41 +23,53 @@ class CountPrediction(nn.Module):
         
         self.t = nn.Parameter(torch.zeros(cell_size, gene_size))
 
-    def S_trans(self, t):
-        return 1 / (1 + torch.exp(-self.k * (t - self.t0_g3 - self.d)))
+    def S_trans(self, gene_index, t):
+        return 1 / (1 + torch.exp(-self.k[gene_index] * (t[:, gene_index] - self.t0_g3[gene_index] - self.d[gene_index])))
     
-    def predict_u(self, alpha, beta, S):
-        tilde_u = ((alpha / beta) * (1 - torch.exp(-beta * self.t)) * (1 - S) +
+    def predict_u(self, gene_index, alpha, beta, S):
+        tilde_u = ((alpha / beta) * (1 - torch.exp(-beta * self.t[:, gene_index])) * (1 - S) +
                    (alpha / beta) * S +
-                   (self.u0_g3 * torch.exp(-beta * (self.t - self.t0_g3)) - (alpha / beta)) * S)
+                   (self.u0_g3[gene_index] * torch.exp(-beta * (self.t[:, gene_index] - self.t0_g3[gene_index])) - (alpha / beta)) * S)
+        for i in range(len(tilde_u)):
+            if torch.isnan(tilde_u[i]):
+                print(f"coeff: {alpha[i]}, {beta[i]}, {S}, {self.t[i][gene_index]}, {self.u0_g3[gene_index]}, {self.t0_g3[gene_index]} ")
         
         return tilde_u
     
-    def predict_s(self, alpha, beta, gamma, S):
-        tilde_s = (((alpha / gamma) * (1 - torch.exp(-gamma * self.t)) +
-                    (alpha / (gamma - beta)) * (torch.exp(-gamma * self.t) - torch.exp(-beta * self.t))) * (1 - S) +
+    def predict_s(self, gene_index, alpha, beta, gamma, S):
+        tilde_s = (((alpha / gamma) * (1 - torch.exp(-gamma * self.t[:, gene_index])) +
+                    (alpha / (gamma - beta)) * (torch.exp(-gamma * self.t[:, gene_index]) - torch.exp(-beta * self.t[:, gene_index]))) * (1 - S) +
                    (alpha / gamma) * S +
-                   (beta * self.u0_g3 / (gamma - beta) * (torch.exp(-gamma * (self.t - self.t0_g3)) - torch.exp(-beta * (self.t - self.t0_g3)))) * S)
+                   (beta * self.u0_g3[gene_index] / (gamma - beta) * (torch.exp(-gamma * (self.t[:, gene_index] - self.t0_g3[gene_index])) - torch.exp(-beta * (self.t[:, gene_index] - self.t0_g3[gene_index])))) * S)
 
         return tilde_s
 
-    def predict(self, out):
-        # [num_cells, num_genes, 3] 
-        alpha = out[:, :, 0] 
-        gamma = out[:, :, 1]  
-        beta = out[:, :, 2]   
+    def predict(self, gene_index, out):
+        # [num_cells, 3] 
+        alpha = out[:, 0] 
+        gamma = out[:, 1]  
+        beta = out[:, 2]   
+
+        with torch.no_grad():
+            for i in range(len(beta)):
+                if beta[i] == 0:
+                    beta[i] += np.random.uniform(1e-6, 1e-5)
+                if beta[i] == gamma[i]:
+                    beta[i] += np.random.uniform(1e-6, 1e-5)
+                if gamma[i] == 0:
+                    gamma[i] += np.random.uniform(1e-6, 1e-5)
 
         # Compute S_trans for each cell
-        S = self.S_trans(self.t)
+        S = self.S_trans(gene_index, self.t)
 
         # Compute tilde_u and tilde_s for each cell
-        tilde_u = self.predict_u(alpha, beta, S)
-        tilde_s = self.predict_s(alpha, beta, gamma, S)
+        tilde_u = self.predict_u(gene_index, alpha, beta, S)
+        tilde_s = self.predict_s(gene_index, alpha, beta, gamma, S)
 
         return tilde_u, tilde_s
 
-    def forward(self, out):
-        return self.predict(out)
+    def forward(self, gene_index, out):
+        return self.predict(gene_index, out)
     
 '''
 GAT Implementation
@@ -73,57 +85,53 @@ Output: alpha, beta, gamma for each cell
     --question: if t should be the same across genes, how to realize this restriction?
 '''
 class GAT(nn.Module):    
-    def __init__(self, n_cell, n_gene, heads):
+    def __init__(self, n_cell, n_gene, layers, heads):
         # 3 out channels, alpha beta gamma (t?)
         super(GAT, self).__init__()
         self.n_cell, self.n_gene = n_cell, n_gene
 
-        self.in_channel_size = 2 # or 3?
-        self.mid_channel_size = 3
-        # could test using more middle layer / different sizes?
-
+        self.layers = layers
         self.out_channel_size = 3
         self.heads = heads # number of attention heads
-        
-        self.convs = nn.ModuleList()
-        self.final_convs = nn.ModuleList()
-        self.bns = nn.ModuleList()
-        for _ in range(self.n_gene):
-            self.convs.append(GATConv(self.in_channel_size, self.mid_channel_size, heads=heads, concat=True, dropout=0.6))
-            self.bns.append(nn.BatchNorm1d(self.heads * self.mid_channel_size))
-            self.final_convs.append(GATConv(self.heads * self.mid_channel_size, self.out_channel_size, heads=1, concat=False, dropout=0.6))
+        self.in_channel_size = 2 # or 3?
+        self.mid_channel_size = 32 * self.heads
+        # could test using more middle layer / different sizes?
+
+        # one module list for one gene
+        self.convlist = [nn.ModuleList() for _ in range(self.n_gene)]
+        self.bnslist = [nn.ModuleList() for _ in range(self.n_gene)]
+        sizes = [self.in_channel_size] + [self.mid_channel_size] * layers + [self.out_channel_size]
+        for i in range(self.n_gene):
+            for j in range(self.layers + 1):
+                self.convlist[i].append(GATConv(sizes[j], sizes[j+1], heads=(self.heads if j != self.layers else 1), concat=(j==self.layers), dropout=0.6))
+                if j != layers:
+                    self.bnslist[i].append(nn.BatchNorm1d(self.mid_channel_size))
 
         self.cp = CountPrediction(self.n_cell, self.n_gene)
 
     def predict(self, gene_data, gene_index):
-        outs = []
-        # for each gene
-        # for i, gene_data in enumerate(gene_list):
         x, edge_index, edge_weight = gene_data.x, gene_data.edge_index, gene_data.edge_attr
         x = x.float()
-        x = F.elu(self.convs[gene_index](x, edge_index))
-        x = self.bns[gene_index](x)
-        x = F.dropout(x, p=0.6, training=self.training)
-        x = self.final_convs[gene_index](x, edge_index)
-        outs.append(x)
-
-        # Combine outputs for all genes, resulting in a tensor of shape [num_cells, num_genes, 3]
-        out = torch.stack(outs, dim=1)
-        return out
+        for i in range(self.layers + 1):
+            x = F.elu(self.convlist[gene_index][i](x, edge_index))
+            if i != self.layers:
+                x = self.bnslist[gene_index][i](x)
+                x = F.dropout(x, p=0.6, training=self.training)
+        return x
     
     def forward(self, gene_data, gene_index):
         # alpha beta gamma's
         out = self.predict(gene_data, gene_index)
-        tilde_u, tilde_s = self.cp(out)
+        tilde_u, tilde_s = self.cp(gene_index, out)
         return tilde_u, tilde_s
 
 # Question - intuitive meaning of this loss?
 # see VeloVi implementation at https://github.com/YosefLab/velovi/blob/main/velovi/_module.py#L488
 def switch_loss(u_switch, s_switch, top_u, top_s):
-    switch_loss = torch.sum((u_switch-top_u)**2 + (s_switch-top_s)**2)
+    switch_loss = torch.sum((u_switch-top_u)**2 + (s_switch-top_s)**2, dtype=torch.float64)
     return switch_loss
 
 def count_loss(pred_u, pred_s, u, s):
-    count_loss = torch.mean((u-pred_u)**2 + (s-pred_s)**2)
+    count_loss = torch.mean((u-pred_u)**2 + (s-pred_s)**2, dtype=torch.float64)
     return count_loss
 
--- train.py
+++ train.py
@@ -4,13 +4,16 @@ import torch
 import model
 import matplotlib.pyplot as plt
 import wandb
+import numpy as np
 
-num_epochs = 10000
+wb = True
+num_epochs = 100000
+lr = 1e-4
 
 def train(dataset):
     dataloader = data.SingleCellLoader(dataset, batch_size=1, shuffle=False)
-    model_gat = model.GAT(n_cell=dataset.n_cell, n_gene=len(dataset), heads=8)
-    optimizer = optim.Adam(model_gat.parameters(), lr=1e-4)
+    model_gat = model.GAT(n_cell=dataset.n_cell, n_gene=len(dataset), layers=32, heads=2)
+    optimizer = optim.Adam(model_gat.parameters(), lr=lr)
     losses = []
 
     for epoch in range(num_epochs):
@@ -24,8 +27,16 @@ def train(dataset):
             u = dataset.get_cells_by_gene(gene_index)[:, 0]
             s = dataset.get_cells_by_gene(gene_index)[:, 1]
             tilde_u, tilde_s = model_gat(gene_graph, gene_index)
+            print(f"shapes: {tilde_u.shape}, {u.shape}")
 
             count_loss_value = model.count_loss(tilde_u, tilde_s, u, s)
+            if torch.isnan(count_loss_value):
+                print("Nan detected.")
+                # np.savetxt('predicted_u.txt', tilde_u.detach().numpy())
+                # np.savetxt('predicted_s.txt', tilde_s.detach().numpy())
+                # np.savetxt('u.txt', u.detach().numpy())
+                # np.savetxt('s.txt', s.detach().numpy())
+                exit(-1)
 
             threshold_u = torch.quantile(u.flatten(), 0.95)
             threshold_s = torch.quantile(s.flatten(), 0.95)
@@ -37,7 +48,10 @@ def train(dataset):
             # switch_loss_value = 0
             total_loss = count_loss_value + switch_loss_value
 
-            wandb.log({"loss": total_loss})
+            if wb:
+                wandb.log({"loss": total_loss})
+            else:
+                print(f"loss: {total_loss}")
 
             total_loss.backward()
             optimizer.step()
@@ -46,7 +60,15 @@ if __name__ == "__main__":
     scrna_path = "chicken_heart\\RNA_D14_adata.h5ad"
     st_path = "chicken_heart\\Visium_D14_adata.h5ad"
 
-    wandb.init(project = "GAT-rna-velo")
+    if wb:
+        wandb.init(project="GAT-rna-velo", config={
+            "learning_rate": lr,
+            "architecture": "CNN",
+            "epochs": num_epochs
+        })
     dataset = data.SingleCellDataset(st_path)
+
     dataset.process_graph()
     train(dataset)
+    if wb:
+        wandb.finish()
